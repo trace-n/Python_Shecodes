bd4e3854bfaca048a27dc06b46282882988b63dc
Tracey Nguyen <trace_n@hotmail.com>
2023-05-30 20:05:02 +1000

    lesson 1 update - initial

---
+++ Session_1/variables.py
@@ -0,0 +1,41 @@
+# comments for program - variable declaration
+first_name = "Tracey"
+last_name = "Nguyen"
+programming_language = "Python"
+
+# Data Types - Text data, numerical data
+# Text data - String requires single or double quotes
+today = "Tuesday"
+# print(today)
+# print(f"Today is {today}")
+# print(type(today))
+
+# Numerical data types - Integer, FLoat
+# Integer - whole number
+# calculations can be performed with numerical data types dynamically
+parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+# height = 153
+# print(f"My height is {height/100}m")
+
+# Float
+weight = 46.5
+# print(type(weight))
+# print(f"My weight is {weight*2.1}in lbs")
+
+distance = "5000"
+# print(int(distance) + 8)
+# print(distance + str(8))
+# print("Hello")
+# input("Hello")
+
+dog_name = input("What's your dog's name?")
+print(f"nice to meet you {dog_name}")
+                 
+# birth_year = input("What year were you born?")
+# print(f"You are {2023-int}")
+
+
+
+
