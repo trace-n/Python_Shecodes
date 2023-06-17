7b04870743134592a4e360b273f04c9105b03eba
Tracey Nguyen <trace_n@hotmail.com>
2023-06-17 22:18:12 +1000

    updated lesson file

--- Session_6/dictionaries.py
+++ Session_6/dictionaries.py
@@ -1,5 +1,144 @@
 import csv
 
+
+# Dictionary
+# Keys are unique in a dictionary
+# Values in dictionary can be any data type
+# Keys can only be immutable data types - cannot be changed
+# Immutable - Strings, integers, floats, booleans
+
+
+student_phonebook = {
+    "Cindy":111,
+    "Tracey":123,
+    "Pauline":444
+}
+                    
+
+# # add key/value to dictionary
+# student_phonebook["Yara"] = 555          
+# print(student_phonebook)
+
+# # change dictionary key value
+# student_phonebook["Cindy"] = 555
+# print(student_phonebook)
+
+# # delete dictionary key/value
+# del student_phonebook["Cindy"]
+# print(student_phonebook)
+
+## default key
+# for element in student_phonebook:
+    # print(element)
+
+# for key in student_phonebook:
+#     print(key, student_phonebook[key])
+
+# for key in student_phonebook.keys():
+#     print(key)
+
+# for value in student_phonebook.values():
+#     print(value)
+
+# # returns tuple eg. ('Cindy', 111)
+# for items in student_phonebook.items():
+#     print(items)
+
+# # can access the tuple values by assigning two vars to split out the tuple returned
+# for key, value in student_phonebook.items():
+#     print(key, value)
+
+# direct assignment of multiple variables to multiple values
+# pack and unpack values
+# a,b,c = [1,2,3]
+# print(a)
+# print(b)
+# print(c)
+
+
+# temp_dict = {
+#     "Mon": 3, 
+#     "Tue": 15, 
+#     "Wed": 15, 
+#     "Thu": 15
+#     }
+
+# print(temp_dict)
+
+# only returns the first instance of max
+# max_temp = max(temp_dict, key=temp_dict.get)
+# print(max_temp)
+
+# to find all the instances of max
+# ----------------------------------
+# max_keys = [key for key, value in temp_dict.items() if value == max(temp_dict.values())]
+# print(max_keys, max(temp_dict.values()))
+
+
+
+temp_dict = {
+    "Mon": {"Min": 30, "Max": 12}, 
+    "Tue": {"Min": 7, "Max": 28},  
+    "Wed": {"Min": 8, "Max": 28},  
+    "Thu": {"Min": 11, "Max": 25},
+    "Fri": {"Min": 7, "Max": 25}    
+    }
+
+# print(temp_dict)
+
+# for key, value in temp_dict.items():
+#     print(key, value)
+
+# for value in temp_dict.values():
+    # print(value["Min"])
+    # print(value["Max"])
+    # print(min(temp_dict.values("Min")))
+    # print(f"{value["Min"]}, {min(temp_dict.values("Min"))})
+
+max_temp = {}
+# max_temp = {key: max(value.values()) for key, value in temp_dict.items()}
+
+max_temp = {key: value["Max"] for key, value in temp_dict.items()}
+# --------------------------------------------------
+# This finds the max temperature 
+# get max temp list largest max value
+max_temp_value = max([value["Max"] for key, value in temp_dict.items()])
+max_temp_keys = [key for key, value in temp_dict.items() if value["Max"] == max_temp_value]
+# ---------------------------------------------------
+# This finds the max temperature 
+# get max temp list largest max value
+min_temp_value = min([value["Min"] for key, value in temp_dict.items()])
+min_temp_keys = [key for key, value in temp_dict.items() if value["Min"] == min_temp_value]
+# ---------------------------------------------------
+
+print(max_temp)
+print("max temp valu",max_temp_value)
+print("max temp keys", max_temp_keys)
+print(max(max_temp.values())) # max temperature
+
+print("min temp valu",min_temp_value)
+print("min temp keys", min_temp_keys)
+
+# max keys
+max_keys = [key for key, value in max_temp.items() if value == max(max_temp.values())]
+print(max_keys)
+
+# max_temp_value = [max(value["Max"].values()) for key, value in temp_dict.items()]
+# print(max_temp_value)
+
+min_temp = {key: min(value.values()) for key, value in temp_dict.items()}
+print(min_temp)
+
+# max_keys = [key for key, value in temp_dict.items() if value == max(temp_dict.values())]
+
+# list comprehension to extract out the min/max and find the max
+
+# max_keys = [key for key, value in temp_dict.items() if value == max(temp_dict.values())]
+# print(max_keys, max(temp_dict.values()))
+
+# max_temp = max(temp_dict, key=temp_dict.get)
+# print(max_temp)
+
 #-----------------------
 # Dictionaries - Exercise 1
 #-----------------------
@@ -35,7 +174,7 @@ colour_counts = {
     "black": 0
     }
 
-# with open("/c/Users/timmi/OneDrive/Documents/she_codes/Python/Python_Shecodes/Session_5/colours_865.csv", mode = "r") as csv_data:
+## with open("/c/Users/timmi/OneDrive/Documents/she_codes/Python/Python_Shecodes/Session_5/colours_865.csv", mode = "r") as csv_data:
 # with open("C:\\Users\\timmi\\OneDrive\\Documents\\she_codes\\Python\\Python_Shecodes\\Session_5\\colours_865.csv", mode = "r") as csv_data:
 
 #     lines = csv.reader(csv_data)
@@ -116,20 +255,20 @@ colour_counts = {
 # Dictionaries - Exercise 5
 #-----------------------
 
-with open("C:\\Users\\timmi\\OneDrive\\Documents\\she_codes\\Python\\Python_Shecodes\\Session_5\\data\\colours_20_simple.csv", mode="r") as csv_data:
-    # read file
-    # skip first line
-    next(csv_data)
-    lines = csv.reader(csv_data)
+# with open("C:\\Users\\timmi\\OneDrive\\Documents\\she_codes\\Python\\Python_Shecodes\\Session_5\\data\\colours_20_simple.csv", mode="r") as csv_data:
+#     # read file
+#     # skip first line
+#     next(csv_data)
+#     lines = csv.reader(csv_data)
     
-    # create empty dictionary
-    colour_dict = {}
+#     # create empty dictionary
+#     colour_dict = {}
 
-    # loop and assign values from row to dictionary
-    # this time using a dictionary as the value for the dictionary key
-    for row in lines:
-        colour_dict[row[1]] = {"RGB": row[0], "English": row[2]}
+#     # loop and assign values from row to dictionary
+#     # this time using a dictionary as the value for the dictionary key
+#     for row in lines:
+#         colour_dict[row[1]] = {"RGB": row[0], "English": row[2]}
     
-    # print output
-    for keys, values in colour_dict.items():
-        print(f"{keys}: {values}")
+#     # print output
+#     for keys, values in colour_dict.items():
+#         print(f"{keys}: {values}")
